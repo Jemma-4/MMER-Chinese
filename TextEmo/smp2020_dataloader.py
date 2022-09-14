e2cc27424f8e11f50283717d683b1bfad99d8599
Jemma-4 <1939336810@qq.com>
2022-09-14 20:33:20 +0800

    新增文本情绪识别模块/smp2020数据集

---
+++ TextEmo/smp2020_dataloader.py
@@ -0,0 +1,85 @@
+from cProfile import label
+from paddlenlp.datasets import DatasetBuilder
+import paddle
+import numpy as np
+
+# 定义数据集对应文件及其文件存储格式
+class EmotionData(DatasetBuilder):
+    SPLITS = {
+        'train': './data/SMP2020/train.csv',  # 训练集
+        'dev': './data/SMP2020/valid.csv',    # 验证集
+        'test': './data/SMP2020/test.csv',    # 测试集
+    }
+
+    def _get_data(self, mode, **kwargs):
+        filename = self.SPLITS[mode]
+        return filename
+
+    def _read(self, filename):
+        """读取数据"""
+        with open(filename, 'r') as f:
+            head = None
+            for line in f:
+                data = line.strip().split("\t")    # 以'\t'分隔各列
+                if not head:
+                    head = data
+                else:
+                    text_a, label = data
+                    yield {"text_a": text_a, "label": label}  # 数据的格式：text_a,label
+
+    def get_labels(self):
+        label_list = ['happy', 'sad', 'neutral', 'fear', 'angry', 'surprise']
+        return label_list   # 类别标签
+
+# 定义数据集加载函数
+def load_dataset(name=None,
+                data_files=None,
+                splits=None,
+                lazy=None,
+                **kwargs):
+
+    reader_cls = EmotionData
+    print(reader_cls)
+    if not name:
+        reader_instance = reader_cls(lazy=lazy, **kwargs)
+    else:
+        reader_instance = reader_cls(lazy=lazy, name=name, **kwargs)
+
+    datasets = reader_instance.read_datasets(data_files=data_files, splits=splits)
+    return datasets
+
+# 定义数据加载和处理函数
+def convert_example(example, tokenizer, max_seq_length=512, is_test=False):
+    qtconcat = example["text_a"]
+    encoded_inputs = tokenizer(text=qtconcat, max_seq_len=max_seq_length)
+    input_ids = encoded_inputs["input_ids"]
+    token_type_ids = encoded_inputs["token_type_ids"]
+
+    if not is_test:
+        label = np.array([example["label"]], dtype="int64")
+        return input_ids, token_type_ids, label
+    else:
+        return input_ids, token_type_ids
+
+# 数据加载函数dataloader
+def create_dataloader(dataset,
+                      mode='train',
+                      batch_size=1,
+                      batchify_fn=None,
+                      trans_fn=None):
+    if trans_fn:
+        dataset = dataset.map(trans_fn)
+
+    shuffle = True if mode == 'train' else False
+    if mode == 'train':
+        batch_sampler = paddle.io.DistributedBatchSampler(
+            dataset, batch_size=batch_size, shuffle=shuffle)
+    else:
+        batch_sampler = paddle.io.BatchSampler(
+            dataset, batch_size=batch_size, shuffle=shuffle)
+
+    return paddle.io.DataLoader(
+        dataset=dataset,
+        batch_sampler=batch_sampler,
+        collate_fn=batchify_fn,
+        return_list=True)
