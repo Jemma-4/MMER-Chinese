474b2138106f118fa895130216fa2e5cd066336e
Jemma-4 <1939336810@qq.com>
2022-09-16 16:40:55 +0800

    更新未上传录音时页面样式

--- preend_2/src/views/AudioItem.vue
+++ preend_2/src/views/AudioItem.vue
@@ -47,7 +47,12 @@
         <el-button type="primary" @click="onSubmit">查询</el-button>
       </div>
 
+      <div v-show="!recUpload" style="height: 100%; padding-top: 100px">
+        提示信息：请您尽可能真实详尽地描述出您当前的感受。
+      </div>
+
       <div
+        v-if="recUpload"
         v-loading="!processReady"
         element-loading-text="模型处理中"
         element-loading-spinner="el-icon-loading"
@@ -78,9 +83,10 @@ export default {
       recStart: false,
       recPause: false,
       recEnd: false,
+      recUpload:false,
       recBtnText: "录音",
       resultData: "",
-      processReady: true,
+      processReady: false,
       audioMD5: "",
       audio_url: "",
       upload_url: baseurl + "uploadAudio/",
@@ -208,6 +214,7 @@ export default {
     },
     // 上传wav格式录音文件，此接口以后要调整到父级页面
     uploadProcess() {
+      this.recUpload = true 
       if (this.recorder) {
         this.uploadProgress();
         let blob = this.recorder.getWAVBlob();
